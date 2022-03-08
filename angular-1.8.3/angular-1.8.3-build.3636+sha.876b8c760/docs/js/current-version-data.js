b995304e6db840a834da021e287affec0ddc8bf1
Veera Venkata Satyanarayana Pinisetti <satyanarayana.pinisetti@hotmail.com>
2022-03-07 23:32:06 -0500

    added angular libraries

---
+++ angular-1.8.3/angular-1.8.3-build.3636+sha.876b8c760/docs/js/current-version-data.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('currentVersionData', [])
+  .value('CURRENT_NG_VERSION', {
+  "raw": "v1.8.2",
+  "major": 1,
+  "minor": 8,
+  "patch": 3,
+  "prerelease": [
+    "build",
+    "3636"
+  ],
+  "build": "sha.876b8c760",
+  "version": "1.8.3-build.3636",
+  "codeName": "snapshot",
+  "isSnapshot": true,
+  "full": "1.8.3-build.3636+sha.876b8c760",
+  "branch": "master",
+  "distTag": "latest",
+  "cdn": {
+    "raw": "v1.8.2",
+    "major": 1,
+    "minor": 8,
+    "patch": 2,
+    "prerelease": [],
+    "build": [],
+    "version": "1.8.2",
+    "docsUrl": "http://code.angularjs.org/1.8.2/docs"
+  }
+});
